b7a7a770b924cd1727f634733a13e25d4cc0af81
ryuchallenger <ryuchallenger@gmail.com>
2024-10-17 01:16:19 +0900

    20241017

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function updateCountdown(weddingDate) {
     const now = new Date();
-    const timeDifference = weddingDate - now; // 시간 차이 계산
+    const timeDifference = now - weddingDate; // 시간 차이 계산
 
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -40,7 +40,7 @@ function updateCountdown(weddingDate) {
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
     let message;
-    if (timeDifference >= 0) {
+    if (timeDifference <= 0) {
         // 결혼식 전까지 남은 시간
         message = `결혼식까지, ${days}일 ${hours}시간 ${minutes}분 ${seconds}초 남았습니다.`;
     } else {
